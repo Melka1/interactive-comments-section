ea6b81c43282d087edef53396e7d8ae7386b15b8
Melka Tolessa <melkatole1@gmail.com>
2023-02-03 01:45:23 +0300

    some additional changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function handleReply(id, type, username){
     let element = ` <div class="comment--reply ${type}">
                         <img src="./images/avatars/image-juliusomo.png" alt="">
                         <textarea name="reply" id="" cols="30" rows="10">@${username} </textarea>
-                        <button class="button--reply">REPLY</button>
+                        <button class="button--reply" onclick="handleReplying('${id}', '${type}')">REPLY</button>
                     </div>`
     $(`#${id}`).after(element)
 }
@@ -12,9 +12,10 @@ function handleEdit(id, type){
     let comment = $(`#${id} .comment`).text()
     let element = '<textarea cols="30" rows="10"></textarea>'
     $(`#${id} .comment`).remove()
-    $(`#${id} .${type}`).append(element)
+    $(`#${id} .comment--content`).append(element)
     $(`#${id} textarea`).val(comment)
-    $(`#${id} .${type}`).parent().append(`<button onclick="handleUpdate('${id}', '${type}')" class="button--update">UPDATE</button>`)
+    console.log(id)
+    $(`#${id}`).children(".comment--container").append(`<button onclick="handleUpdate('${id}', '${type}')" class="button--update">UPDATE</button>`)
 }
 
 function handleUpdate(id, type){
@@ -23,4 +24,52 @@ function handleUpdate(id, type){
     $(`#${id} textarea`).remove()
     $(`#${id} .button--update`).remove()
     $(`#${id} .comment`).text(comment)
+}
+
+function handleReplying(id, type){
+    let i = 1;
+    while(document.querySelector(`#${id}rply${i}`)){
+        i++;
+    }
+
+    let kind = id
+    let number = (type=="sub")?kind.replace("submsg", ""):kind.replace("msg","")
+    let num = parseInt(number)
+    console.log(number, num)
+    let comment = $(`#${id}`).next().children('textarea').val()
+    console.log(comment)
+    $(`#${id}`).next().remove()
+    let commentSection = `
+    <div class="comment--section ${type}"  id="${id}rply${i}">
+        <div class="score">
+        <img src="./images/icon-plus.svg" alt="" class="plus">
+        <p class="score--number">0</p>
+        <img src="./images/icon-minus.svg" alt="" class="minus">
+        </div>
+        <div class="comment--container">
+        <div class="header">
+            <img class="profile--pic" src="./images/avatars/image-juliusomo.png" alt="">
+            <p class="name">juliusomo</p>
+            <p class="who">you</p>
+            <p class="timestamp">Now</p>
+            <div class="controls">
+            <div class="delete">
+                <img src="./images/icon-delete.svg" alt="">
+                <p>Delete</p>
+            </div>
+            <div class="edit"  onclick="handleEdit('${id}rply${i}', '${type}')">
+                <img src="./images/icon-edit.svg" alt="" >
+                <p>Edit</p>
+            </div>
+            <div class="reply">
+            </div>
+            </div>
+        </div>
+        <div class="comment--content">
+            <p class="comment"></p>
+        </div>
+        </div>
+    </div>`;
+  $(`#${id}`).after(commentSection)
+  $(`#${id}rply${i} .comment`).text(comment)
 }
