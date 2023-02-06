bd1baeb8bcf59c2914fb6800f3f56a29feabffb0
Melka Tolessa <melkatole1@gmail.com>
2023-02-06 05:22:08 +0300

    applied some restrictions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let svgPlus = (id) => `<svg onclick='add("${id}")' width="11" height="11" xmlns="http://www.w3.org/2000/svg"><path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"/></svg>`
-let svgMinus = (id) => `<svg onclick='minus("${id}")' width="11" height="3" xmlns="http://www.w3.org/2000/svg"><path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"/></svg>`
+let svgPlus = (id) => `<button onclick='add("${id}")'><svg width="11" height="11" xmlns="http://www.w3.org/2000/svg"><path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"/></svg></button>`
+let svgMinus = (id) => `<button onclick='minus("${id}")'><svg width="11" height="3" xmlns="http://www.w3.org/2000/svg"><path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"/></svg></button>`
 let svgReply = `<svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z"/></svg>`
 let svgDelete = `<svg width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M1.167 12.448c0 .854.7 1.552 1.555 1.552h6.222c.856 0 1.556-.698 1.556-1.552V3.5H1.167v8.948Zm10.5-11.281H8.75L7.773 0h-3.88l-.976 1.167H0v1.166h11.667V1.167Z"/></svg>`
 let svgEdit = `<svg width="14" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M13.479 2.872 11.08.474a1.75 1.75 0 0 0-2.327-.06L.879 8.287a1.75 1.75 0 0 0-.5 1.06l-.375 3.648a.875.875 0 0 0 .875.954h.078l3.65-.333c.399-.04.773-.216 1.058-.499l7.875-7.875a1.68 1.68 0 0 0-.061-2.371Zm-2.975 2.923L8.159 3.449 9.865 1.7l2.389 2.39-1.75 1.706Z"/></svg>`
@@ -27,6 +27,8 @@ function handleReply(id, type, username){
                         </div>
                     </div>`
     $(`#${id}`).after(element)
+    $('.comment--section .controls button').attr("disabled", true)
+    $('.comment--section .score button').attr("disabled", true)
 }
 
 function handleEdit(id, type){
@@ -37,6 +39,8 @@ function handleEdit(id, type){
     $(`#${id} textarea`).val(comment)
     console.log(id)
     $(`#${id} .comment--content`).append(`<button onclick="handleUpdate('${id}', '${type}')" class="button--update">UPDATE</button>`)
+    $('.comment--section .controls button').attr("disabled", true)
+    $('.comment--section .score button').attr("disabled", true)
 }
 
 function handleUpdate(id, type){
@@ -45,6 +49,8 @@ function handleUpdate(id, type){
     $(`#${id} textarea`).remove()
     $(`#${id} .button--update`).remove()
     $(`#${id} .comment`).html(handleUser(comment))
+    $('.comment--section .controls button').attr("disabled", false)
+  $('.comment--section .score button').attr("disabled", false)
 }
 
 function handleUser(comment){
@@ -92,14 +98,14 @@ function handleReplying(id, type){
         </div>
             
         <div class="controls">
-            <div class="delete" onclick="handleDelete('${id}rply${i}')">
+            <button class="delete" onclick="handleDelete('${id}rply${i}')">
                 ${svgDelete}
                 <p>Delete</p>
-            </div>
-            <div class="edit"  onclick="handleEdit('${id}rply${i}', '${type}')">
+            </button>
+            <button class="edit"  onclick="handleEdit('${id}rply${i}', '${type}')">
                 ${svgEdit}
                 <p>Edit</p>
-            </div>
+            </button>
         </div>
 
         <div class="comment--content">
@@ -109,7 +115,10 @@ function handleReplying(id, type){
     </div>`;
   $(`#${id}`).after(commentSection)
   $(`#${id}rply${i} .comment`).html(handleUser(comment))
+  $('.comment--section .controls button').attr("disabled", false)
+  $('.comment--section .score button').attr("disabled", false)
 }
+
 
 function handleSend() {
     let user = JSON.parse(localStorage.getItem("data")).currentUser;
@@ -216,19 +225,20 @@ const newComment = async()=>{
                         
                     <div class="controls">
                         ${data.currentUser.username==com.user.username?
-                            `<div class="delete" onclick="handleDelete('mainmsg${index}')">
-                                ${svgDelete}
-                                <p>Delete</p>
-                            </div>
-                            <div class="edit"  onclick="handleEdit('mainmsg${index}', 'main')">
-                                ${svgEdit}
-                                <p>Edit</p>
-                            </div>
-                            </div>`:
-                            `<div class="reply" onclick="handleReply('mainmsg${index}', 'main', '${com.user.username}')" >
-                                ${svgReply}
-                                <p>Reply</p>
-                        </div>`}
+                        `<button class="delete" onclick="handleDelete('mainmsg${index}')">
+                            ${svgDelete}
+                            <p>Delete</p>
+                        </button>
+                        <button class="edit"  onclick="handleEdit('mainmsg${index}', 'main')">
+                            ${svgEdit}
+                            <p>Edit</p>
+                        </button>
+                        `:
+                        `<button class="reply" onclick="handleReply('mainmsg${index}', 'main', '${com.user.username}')" >
+                            ${svgReply}
+                            <p>Reply</p>
+                        </button>
+                        `}
                     </div>
 
                     <div class="comment--content">
@@ -258,18 +268,18 @@ const newComment = async()=>{
 
                             <div class="controls">
                                 ${data.currentUser.username==reply.user.username?
-                                `<div class="delete" onclick="handleDelete('mainmsg${index}submsg${ind}')">
+                                `<button class="delete" onclick="handleDelete('mainmsg${index}submsg${ind}')">
                                     ${svgDelete}
                                     <p>Delete</p>
-                                </div>
-                                <div class="edit"  onclick="handleEdit('mainmsg${index}submsg${ind}', 'sub')">
+                                </button>
+                                <button class="edit"  onclick="handleEdit('mainmsg${index}submsg${ind}', 'sub')">
                                     ${svgEdit}
                                     <p>Edit</p>
-                                </div>`:
-                                `<div class="reply" onclick="handleReply('mainmsg${index}submsg${ind}', 'sub', '${reply.user.username}')" >
+                                </button>`:
+                                `<button class="reply" onclick="handleReply('mainmsg${index}submsg${ind}', 'sub', '${reply.user.username}')" >
                                     ${svgReply}
                                     <p>Reply</p>
-                                </div>`}
+                                </button>`}
                             </div>
                             <div class="comment--content">
                                 <p class="comment">${handleUser(`@${reply.replyingTo}, ${reply.content}`)}</p>
